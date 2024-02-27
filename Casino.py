4691f7c07ad7c1633eb3f65b743f3f484a9ed99c
dogasp <37941592+dogasp@users.noreply.github.com>
2024-02-27 18:43:12 +0100

    Update Casino.py

--- Casino.py
+++ Casino.py
@@ -6,21 +6,20 @@ def main():
         def __init__(self, title, fieldList):
             self.data.title = title
             self.data.fieldList = fieldList # A list of field from wich you can vote
-            self.data.toDisplay = ""
-            self.data.winners = []
-            sp.cast(self.data.winners, sp.list[sp.address])
+            self.data.winners = sp.set()
+            sp.cast(self.data.winners, sp.set[sp.address])
 
         @sp.entrypoint
-        def vote(self, option, address):
+        def vote(self, address, option):
             assert self.data.fieldList.contains(option), "Field not recognized"
-            self.data.fieldList[option].push(address)
+            self.data.fieldList[option].add(address)
 
         @sp.entrypoint
         def result(self):
-            lowestAddress = []
+            lowestAddress = sp.set()
             lowestCount = 9999999999999999
             for key in self.data.fieldList.keys():
-                if len(self.data.fieldList[key]) > lowestCount:
+                if len(self.data.fieldList[key]) < lowestCount:
                     lowestCount = len(self.data.fieldList[key])
                     lowestAddress = self.data.fieldList[key]
             self.data.winners = lowestAddress
@@ -32,26 +31,20 @@ def test():
     bob = sp.test_account('Bob').address
     eve = sp.test_account('Eve').address
     
-    contract = main.Bet("Do you like Tezos", {"Yes": [], "No": []})
+    contract = main.Bet("Do you like Tezos", {"Yes": sp.set(), "No": sp.set()})
     scenario += contract
 
+    contract.vote(sp.record(address = alice, option = "Maybe"), _valid = False, _exception = "Field not recognized")
+    contract.vote(sp.record(address = alice, option = "Yes"))
+    contract.vote(sp.record(address = bob, option = "No"))
+    contract.vote(sp.record(address = alice, option = "No"))
+
+    contract.result()
+    
+    
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
